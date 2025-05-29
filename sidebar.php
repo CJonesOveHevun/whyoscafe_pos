ee997fcc6c74962e2e4fe307c87db2bf3007e3f5
CJonesOveHevun <jeromemojica10@gmail.com>
2025-05-29 20:11:22 +0800

    fix sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <aside class="sidebar">
     <h2>Whyos Cafe</h2>
     <p class="tagline">Inventory System</p>
@@ -12,18 +11,3 @@
         <p><strong>Store Manager</strong><br><span>Admin</span></p>
     </div>
 </aside>
-=======
-<aside class="sidebar">
-    <h2>Whyos Cafe</h2>
-    <p class="tagline">Inventory System</p>
-    <nav>
-        <a href="#">Dashboard</a>
-        <a href="#">Inventory</a>
-        <a class="active" href="#">Ingredients Calculator</a>
-        <a href="#">Local Events</a>
-    </nav>
-    <div class="footer">
-        <p><strong>Store Manager</strong><br><span>Admin</span></p>
-    </div>
-</aside>
->>>>>>> b6402ea (added backend logic for recipe calculator)
