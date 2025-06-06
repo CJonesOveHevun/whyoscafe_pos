71dfbfece4a7a7e76a3a7de910f5a283f543a8db
CJonesOveHevun <96277517+CJonesOveHevun@users.noreply.github.com>
2025-06-07 01:15:37 +0800

    Update db_connect.php

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable('..');
 $dotenv->load();
 
 $mongoUri = $_ENV['MONGO_URI'];
